60aa8261235f4dea5437e605f4fa43ccd1e3c09e
ansal-exp <32477006+ansal-exp@users.noreply.github.com>
2019-04-03 15:21:27 +0530

    check for undefined strings

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function tiny(string) {
+  if (!string) throw new TypeError("Tiny wants a string!");
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
 };
